088dc0d35d9c72c3426f65d7c778d4d2c2e13ab2
jichuyaaaa <lalapaaaa22@gmail.com>
2026-01-14 14:56:14 +0700

    tampil jumlah lulus

--- OneDrive/Documents/night lamp/dagdigdug/Score.java
+++ OneDrive/Documents/night lamp/dagdigdug/Score.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class yourscore  {
+public class Score  {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
--- OneDrive/Documents/night lamp/dagdigdug/score,.java
+++ OneDrive/Documents/night lamp/dagdigdug/score,.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class yourscore  {
+public class score,  {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
